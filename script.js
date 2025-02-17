d4568efc6dea118b14efdd520035c1d347d73150
Prathamesh-265 <prathameshkatre26@gmail.com>
2025-02-17 22:43:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function updateClock() {
-    let headingEl = document.getElementById("heading");
+    
     // Creating date object
     let date = new Date();
     let getHours = date.getHours();
     let getSeconds = date.getSeconds();
     let getMinutes = date.getMinutes();
     let ampm = getHours >= 12 ? "PM" : "AM";
+    
     //Converting hours
     let convertedHours = getHours % 12;
     getMinutes = getMinutes < 10 ? '0' + getMinutes : getMinutes;
     getSeconds = getSeconds < 10 ? '0' + getSeconds : getSeconds;
+    
     //Using template literals
     let str = ` ${convertedHours}:${getMinutes}:${getSeconds} ${ampm}`;
     headingEl.innerText = str;
